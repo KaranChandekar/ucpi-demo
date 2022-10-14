463a631a9ca80eb437bbfaa19716857a0bfbc975
Karan <karanchandekar431@gmail.com>
2022-10-14 19:44:54 +0530

    adding components to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Header } from "./Components/Header/Header";
+import { Section1 } from "./Components/Section1/Section1";
+import { Section2 } from "./Components/Section2/Section2";
+import { Section3 } from "./Components/Section3/Section3";
+import { Section4 } from "./Components/Section4/Section4";
+import { Section5 } from "./Components/Section5/Section5";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
     </div>
   );
 }
