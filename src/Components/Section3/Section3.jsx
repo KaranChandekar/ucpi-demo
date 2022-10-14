9bc62d9608fe08a34e9f046c1de98eaade807f72
Karan <karanchandekar431@gmail.com>
2022-10-14 19:48:20 +0530

    created and styled section3

---
+++ src/Components/Section3/Section3.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import bg2 from "../images/bg4.svg";
+import "./Section3.css";
+
+export const Section3 = () => {
+  return (
+    <div className="section-3" id="section-3">
+      <img className="bg-img" src={bg2} alt="background img" />
+      <div className="text-container">
+        <h2>About Us</h2>
+        <h1>Who we are?</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et
+          distinctio magni sequi perspiciatis, accusantium consequuntur nulla?
+          Nulla, obcaecati!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vero
+          quasi accusantium unde dolores? Dolore ipsa quia quasi eos maxime
+        </p>
+        <p>
+          <span>Join and entrust your financial development to us.</span>
+        </p>
+        <button className="btn btn-1">Get Started</button>
+      </div>
+    </div>
+  );
+};
