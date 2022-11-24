57d50e9c384bd799e401f4bf6ae76c26cb420b98
Karan <karanchandekar431@gmail.com>
2022-11-24 22:34:08 +0530

    update contact section

--- src/Components/KnowMore/KnowMore.jsx
+++ src/Components/KnowMore/KnowMore.jsx
@@ -7,11 +7,9 @@ export const KnowMore = () => {
     <section className="know-more-section" id="know-more">
       <img className="bg-img" src={contactImg} alt="background img" />
       <div className="text-container">
-        <h2>UPI for crypto Payments.</h2>
+        <p className="sub-heading">UPI for crypto Payments.</p>
         <h1>Want to know more about UCPI?</h1>
-        <p>
-          <span>Talk directly with us on linkedin.</span>
-        </p>
+        <p className="about-para">Talk directly with us on linkedin.</p>
         <div className="btn-container">
           <button className="btn btn-1">TALK TO DIGENDRA</button>
           <button className="btn btn-2">TALK TO PRANJAL</button>
